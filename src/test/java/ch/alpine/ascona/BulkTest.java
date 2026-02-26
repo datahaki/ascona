ff409bce8d9e11ffdbf2c35c62a89025674702ca
datahaki <jan.hakenberg@gmail.com>
2026-02-26 18:32:49 +0100

    instance record

--- src/test/java/ch/alpine/ascona/BulkTest.java
+++ src/test/java/ch/alpine/ascona/BulkTest.java
@@ -2,17 +2,22 @@
 package ch.alpine.ascona;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Test;
 
 import ch.alpine.ascony.win.AbstractDemo;
 import ch.alpine.bridge.cgr.InstanceDiscovery;
+import ch.alpine.bridge.cgr.InstanceRecord;
 
 class BulkTest {
   @Test
   void simple() {
-    List<Supplier<AbstractDemo>> list = InstanceDiscovery.of(getClass().getPackageName(), AbstractDemo.class);
+    List<InstanceRecord<AbstractDemo>> list = //
+        InstanceDiscovery.of(getClass().getPackageName(), AbstractDemo.class);
+    list.forEach(e -> {
+      IO.println(e.friendly());
+      IO.println(e.toString());
+    });
     IO.println(list.size());
   }
 }
