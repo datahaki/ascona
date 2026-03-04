34d5cc96a3352c7e1959cd835261191b4a29d726
datahaki <jan.hakenberg@gmail.com>
2026-03-04 12:06:47 +0100

    llp

--- src/main/java/ch/alpine/ascona/LocalLaunchPad.java
+++ src/main/java/ch/alpine/ascona/LocalLaunchPad.java
@@ -5,10 +5,10 @@ import ch.alpine.bridge.io.FileBlock;
 import ch.alpine.bridge.io.ResourceLocator;
 import ch.alpine.bridge.pro.RunLaunchPad;
 
-enum AsconaLaunchPad {
+enum LocalLaunchPad {
   ;
   static void main() {
-    if (!FileBlock.of(ResourceLocator.of(AsconaLaunchPad.class).resolve("")))
-      RunLaunchPad.create(AsconaLaunchPad.class.getPackageName()).runStandalone();
+    if (!FileBlock.of(ResourceLocator.of(LocalLaunchPad.class).resolve("")))
+      RunLaunchPad.create(LocalLaunchPad.class.getPackageName()).runStandalone();
   }
 }
