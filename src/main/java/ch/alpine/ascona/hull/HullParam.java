7e677a8a500f091b89d6058ee3a09014e96445ae
datahaki <jan.hakenberg@gmail.com>
2026-03-19 07:41:22 +0100

    hull

---
+++ src/main/java/ch/alpine/ascona/hull/HullParam.java
@@ -0,0 +1,14 @@
+// code by jph
+package ch.alpine.ascona.hull;
+
+import ch.alpine.bridge.ref.ann.ReflectionMarker;
+import ch.alpine.tensor.img.ColorDataGradients;
+
+@ReflectionMarker
+public class HullParam {
+  public final MeshParam meshParam = new MeshParam();
+  public Boolean cuboid = false;
+  public final RotParam rotParam = new RotParam();
+  public ColorDataGradients cdg = ColorDataGradients.ALPINE;
+  /** IMPORTANT: the value shuffle == true is used to initialized */
+}
